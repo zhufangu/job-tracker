495fd78df14bade143ffc48f1901ffe6813e26b7
zhufangu <75535393+zhufangu@users.noreply.github.com>
2024-11-09 20:04:23 -0600

    Add Logo component and update Landing and Error pages

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,28 @@
 import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Page not found</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to="/dashboard">Back Home </Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 };
 export default Error;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 import Wrapper from '../assets/wrappers/LandingPage';
 import main from '../assets/images/main.svg';
-import logo from '../assets/images/logo.svg';
 import { Link } from 'react-router-dom';
+import Logo from '../components/Logo';
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
