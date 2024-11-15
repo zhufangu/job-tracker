76fd960ab0aef53c623cb01b8c077ebffe296614
zhufangu <75535393+zhufangu@users.noreply.github.com>
2024-11-14 22:33:24 -0600

    Add server setup with Express and Nodemon

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('server is working...');
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  // means when we hit the root URL, we will get the response
+  res.send('Hello World'); // response will be Hello World
+});
+
+app.listen(5100, () => {
+  // server will run on port 5100
+  console.log('Server is running on http://localhost:5100');
+});
